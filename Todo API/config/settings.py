e74265a0a4013ffddcea46b8bb9e372c0eed0d3e
Sam <sw.waithakasamuel@gmail.com>
2025-01-09 11:29:52 +0300

    Configure CORS headers for API access
    - Added `django-cors-headers==4.6.0` to the project dependencies.
    - Included `corsheaders` in the `INSTALLED_APPS` list.
    - Updated middleware to include `CorsMiddleware` for handling CORS requests.
    - Configured `CORS_ALLOWED_ORIGINS` to allow requests from ports 3000 and 8000.
    - Added `CSRF_TRUSTED_ORIGINS` to include port 3000 for trusted CSRF requests.

--- Todo API/config/settings.py
+++ Todo API/config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     # Third Party Apps
     'rest_framework',
+    'corsheaders',
+
     # Local Apps
     'todos.apps.TodosConfig',
 ]
@@ -56,6 +58,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,3 +138,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+]
