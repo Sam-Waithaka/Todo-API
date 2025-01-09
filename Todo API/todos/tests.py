f0eea083c04c63675a711907c838f46ba6e84ecf
Sam <sw.waithakasamuel@gmail.com>
2025-01-09 10:52:28 +0300

    Add API endpoint tests for Todo list and detail views
    - Added `test_api_listview` to test the API endpoint for listing all Todo items.
    - Added `test_api_detailview` to test the API endpoint for retrieving a specific Todo item by its ID.
    - Ensured both tests validate the correct status codes and response content.

--- Todo API/todos/tests.py
+++ Todo API/todos/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 from .models import Todo
 
@@ -17,3 +20,15 @@ class TodoModelTest(TestCase):
         todo = Todo.objects.get(id=1)
         expected_object_name = f'{todo.body}'
         self.assertEqual(expected_object_name, 'a body here')
+
+    def test_api_listview(self):
+        response = self.client.get(reverse('todo_list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)  
+        self.assertContains(response, 'first todo')
+
+    def test_api_detailview(self):
+        response = self.client.get(reverse('todo_detail', kwargs={'pk': 1}), format='json') 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, 'first todo')
+        
